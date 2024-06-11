6615159369794bdaa76a66412eea4a9068dd8153
Amit Purohit <amit.purohit@nxp.com>
2024-06-11 16:06:20 -0700

    cproject files and board.h

--- sdcd_motion_wakeup/tap_detection/frdmmcxn947/accel4_click/frdmmcxn947_a8974_click_tap_motion_wakeup/board/board.h
+++ sdcd_motion_wakeup/tap_detection/frdmmcxn947/accel4_click/frdmmcxn947_a8974_click_tap_motion_wakeup/board/board.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 NXP
+ * Copyright 2022-2023 NXP
  * All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
@@ -14,7 +14,7 @@
  * Definitions
  ******************************************************************************/
 /*! @brief The board name */
-#define BOARD_NAME "MCX-N9XX-EVK"
+#define BOARD_NAME "FRDM-MCXN947"
 
 /*! @brief The UART to use for debug messages. */
 #define BOARD_DEBUG_UART_TYPE       kSerialPort_Uart
@@ -72,10 +72,7 @@
 
 /*! @brief Indexes of the TSI channels for on-board electrodes */
 #ifndef BOARD_TSI_ELECTRODE_1
-#define BOARD_TSI_ELECTRODE_1 19
-#endif
-#ifndef BOARD_TSI_ELECTRODE_2
-#define BOARD_TSI_ELECTRODE_2 21U
+#define BOARD_TSI_ELECTRODE_1 3U
 #endif
 
 /*! @brief Indexes of the TSI mutual channels for FRDM-TOUCH board */
@@ -83,31 +80,31 @@
 #define BOARD_TSI_MUTUAL_RX_ELECTRODE_1 14U
 
 #ifndef BOARD_LED_RED_GPIO
-#define BOARD_LED_RED_GPIO GPIO3
+#define BOARD_LED_RED_GPIO GPIO0
 #endif
 #ifndef BOARD_LED_RED_GPIO_PIN
-#define BOARD_LED_RED_GPIO_PIN 4U
+#define BOARD_LED_RED_GPIO_PIN 10U
 #endif
 
 #ifndef BOARD_LED_BLUE_GPIO
-#define BOARD_LED_BLUE_GPIO GPIO3
+#define BOARD_LED_BLUE_GPIO GPIO1
 #endif
 #ifndef BOARD_LED_BLUE_GPIO_PIN
-#define BOARD_LED_BLUE_GPIO_PIN 3U
+#define BOARD_LED_BLUE_GPIO_PIN 2U
 #endif
 
 #ifndef BOARD_LED_GREEN_GPIO
-#define BOARD_LED_GREEN_GPIO GPIO3
+#define BOARD_LED_GREEN_GPIO GPIO0
 #endif
 #ifndef BOARD_LED_GREEN_GPIO_PIN
-#define BOARD_LED_GREEN_GPIO_PIN 2U
+#define BOARD_LED_GREEN_GPIO_PIN 27U
 #endif
 
 #ifndef BOARD_SW2_GPIO
 #define BOARD_SW2_GPIO GPIO0
 #endif
 #ifndef BOARD_SW2_GPIO_PIN
-#define BOARD_SW2_GPIO_PIN 29U
+#define BOARD_SW2_GPIO_PIN 23U
 #endif
 #define BOARD_SW2_NAME        "SW2"
 #define BOARD_SW2_IRQ         GPIO00_IRQn
@@ -123,56 +120,47 @@
 #define BOARD_SW3_IRQ         GPIO00_IRQn
 #define BOARD_SW3_IRQ_HANDLER GPIO00_IRQHandler
 
-#ifndef BOARD_SW4_GPIO
-#define BOARD_SW4_GPIO GPIO1
-#endif
-#ifndef BOARD_SW4_GPIO_PIN
-#define BOARD_SW4_GPIO_PIN 3U
-#endif
-#define BOARD_SW4_NAME        "SW4"
-#define BOARD_SW4_IRQ         GPIO10_IRQn
-#define BOARD_SW4_IRQ_HANDLER GPIO10_IRQHandler
-
 /* USB PHY condfiguration */
 #define BOARD_USB_PHY_D_CAL     (0x04U)
 #define BOARD_USB_PHY_TXCAL45DP (0x07U)
 #define BOARD_USB_PHY_TXCAL45DM (0x07U)
 
+#define BOARD_HAS_NO_CTIMER_OUTPUT_PIN_CONNECTED_TO_LED (1)
+
 /* Board led color mapping */
-#define LOGIC_LED_ON  1U
-#define LOGIC_LED_OFF 0U
+#define LOGIC_LED_ON  0U
+#define LOGIC_LED_OFF 1U
 
 #define LED_RED_INIT(output)                                           \
     GPIO_PinWrite(BOARD_LED_RED_GPIO, BOARD_LED_RED_GPIO_PIN, output); \
     BOARD_LED_RED_GPIO->PDDR |= (1U << BOARD_LED_RED_GPIO_PIN)                         /*!< Enable target LED_RED */
-#define LED_RED_ON()  GPIO_PortSet(BOARD_LED_RED_GPIO, 1U << BOARD_LED_RED_GPIO_PIN)   /*!< Turn on target LED_RED */
-#define LED_RED_OFF() GPIO_PortClear(BOARD_LED_RED_GPIO, 1U << BOARD_LED_RED_GPIO_PIN) /*!< Turn off target LED_RED */
-#define LED_RED_TOGGLE() \
-    GPIO_PortToggle(BOARD_LED_RED_GPIO, 1U << BOARD_LED_RED_GPIO_PIN) /*!< Toggle on target LED_RED */
+#define LED_RED_ON()     GPIO_PortClear(BOARD_LED_RED_GPIO, 1U << BOARD_LED_RED_GPIO_PIN)  /*!< Turn on target LED_RED */
+#define LED_RED_OFF()    GPIO_PortSet(BOARD_LED_RED_GPIO, 1U << BOARD_LED_RED_GPIO_PIN)    /*!< Turn off target LED_RED */
+#define LED_RED_TOGGLE() GPIO_PortToggle(BOARD_LED_RED_GPIO, 1U << BOARD_LED_RED_GPIO_PIN) /*!< Toggle on target LED_RED */
 
 #define LED_BLUE_INIT(output)                                            \
     GPIO_PinWrite(BOARD_LED_BLUE_GPIO, BOARD_LED_BLUE_GPIO_PIN, output); \
     BOARD_LED_BLUE_GPIO->PDDR |= (1U << BOARD_LED_BLUE_GPIO_PIN)                       /*!< Enable target LED_BLUE */
-#define LED_BLUE_ON() GPIO_PortSet(BOARD_LED_BLUE_GPIO, 1U << BOARD_LED_BLUE_GPIO_PIN) /*!< Turn on target LED_BLUE */
-#define LED_BLUE_OFF() \
-    GPIO_PortClear(BOARD_LED_BLUE_GPIO, 1U << BOARD_LED_BLUE_GPIO_PIN) /*!< Turn off target LED_BLUE */
-#define LED_BLUE_TOGGLE() \
-    GPIO_PortToggle(BOARD_LED_BLUE_GPIO, 1U << BOARD_LED_BLUE_GPIO_PIN) /*!< Toggle on target LED_BLUE */
+#define LED_BLUE_ON()     GPIO_PortClear(BOARD_LED_BLUE_GPIO, 1U << BOARD_LED_BLUE_GPIO_PIN)  /*!< Turn on target LED_BLUE */
+#define LED_BLUE_OFF()    GPIO_PortSet(BOARD_LED_BLUE_GPIO, 1U << BOARD_LED_BLUE_GPIO_PIN)    /*!< Turn off target LED_BLUE */
+#define LED_BLUE_TOGGLE() GPIO_PortToggle(BOARD_LED_BLUE_GPIO, 1U << BOARD_LED_BLUE_GPIO_PIN) /*!< Toggle on target LED_BLUE */
 
 #define LED_GREEN_INIT(output)                                             \
     GPIO_PinWrite(BOARD_LED_GREEN_GPIO, BOARD_LED_GREEN_GPIO_PIN, output); \
     BOARD_LED_GREEN_GPIO->PDDR |= (1U << BOARD_LED_GREEN_GPIO_PIN) /*!< Enable target LED_GREEN */
-#define LED_GREEN_ON() \
-    GPIO_PortSet(BOARD_LED_GREEN_GPIO, 1U << BOARD_LED_GREEN_GPIO_PIN) /*!< Turn on target LED_GREEN */
-#define LED_GREEN_OFF() \
-    GPIO_PortClear(BOARD_LED_GREEN_GPIO, 1U << BOARD_LED_GREEN_GPIO_PIN) /*!< Turn off target LED_GREEN */
-#define LED_GREEN_TOGGLE() \
-    GPIO_PortToggle(BOARD_LED_GREEN_GPIO, 1U << BOARD_LED_GREEN_GPIO_PIN) /*!< Toggle on target LED_GREEN */
+#define LED_GREEN_ON()     GPIO_PortClear(BOARD_LED_GREEN_GPIO, 1U << BOARD_LED_GREEN_GPIO_PIN)  /*!< Turn on target LED_GREEN */
+#define LED_GREEN_OFF()    GPIO_PortSet(BOARD_LED_GREEN_GPIO, 1U << BOARD_LED_GREEN_GPIO_PIN)    /*!< Turn off target LED_GREEN */
+#define LED_GREEN_TOGGLE() GPIO_PortToggle(BOARD_LED_GREEN_GPIO, 1U << BOARD_LED_GREEN_GPIO_PIN) /*!< Toggle on target LED_GREEN */
 
 /* Display. */
 #define BOARD_LCD_DC_GPIO      GPIO0
 #define BOARD_LCD_DC_GPIO_PORT 0U
 #define BOARD_LCD_DC_GPIO_PIN  10U
+
+/* Camera */
+#define BOARD_CAMERA_I2C_BASEADDR         LPI2C7
+#define BOARD_CAMERA_I2C_INSTANCE         7
+#define BOARD_CAMERA_I2C_CLOCK_FREQ       CLOCK_GetLPFlexCommClkFreq(BOARD_CAMERA_I2C_INSTANCE)
 
 /* Serial MWM WIFI */
 #define BOARD_SERIAL_MWM_PORT_CLK_FREQ CLOCK_GetFlexCommClkFreq(2)
@@ -188,24 +176,24 @@
 
 /* ERPC LPSPI configuration */
 #define ERPC_BOARD_LPSPI_SLAVE_READY_USE_GPIO (1)
-#define ERPC_BOARD_LPSPI_BASEADDR             LPSPI1
+#define ERPC_BOARD_LPSPI_BASEADDR             LPSPI3
 #define ERPC_BOARD_LPSPI_BAUDRATE             500000U
-#define ERPC_BOARD_LPSPI_CLKSRC               kCLOCK_Flexcomm1
+#define ERPC_BOARD_LPSPI_CLKSRC               kCLOCK_Flexcomm3
 #define ERPC_BOARD_LPSPI_CLK_FREQ             12000000 // CLOCK_GetFlexCommClkFreq(1)
-#define ERPC_BOARD_LPSPI_INT_GPIO             GPIO4
-#define ERPC_BOARD_LPSPI_INT_PIN              0U
-#define ERPC_BOARD_LPSPI_INT_PIN_IRQ          PIN_INT4_IRQn
-#define ERPC_BOARD_LPSPI_INT_PIN_IRQ_HANDLER  PIN_INT4_IRQHandler
+#define ERPC_BOARD_LPSPI_INT_GPIO             GPIO0
+#define ERPC_BOARD_LPSPI_INT_PIN              16U
+#define ERPC_BOARD_LPSPI_INT_PIN_IRQ          PIN_INT0_IRQn
+#define ERPC_BOARD_LPSPI_INT_PIN_IRQ_HANDLER  PIN_INT0_IRQHandler
 
 /* ERPC LPI2C configuration */
-#define ERPC_BOARD_LPI2C_BASEADDR            LPI2C2_BASE
+#define ERPC_BOARD_LPI2C_BASEADDR            LPI2C0_BASE
 #define ERPC_BOARD_LPI2C_BAUDRATE            100000U
-#define ERPC_BOARD_LPI2C_CLKSRC              kCLOCK_Flexcomm2
+#define ERPC_BOARD_LPI2C_CLKSRC              kCLOCK_Flexcomm0
 #define ERPC_BOARD_LPI2C_CLK_FREQ            12000000 // CLOCK_GetFlexCommClkFreq(2)
-#define ERPC_BOARD_LPI2C_INT_GPIO            GPIO0
-#define ERPC_BOARD_LPI2C_INT_PIN             24U
-#define ERPC_BOARD_LPI2C_INT_PIN_IRQ         PIN_INT0_IRQn
-#define ERPC_BOARD_LPI2C_INT_PIN_IRQ_HANDLER PIN_INT0_IRQHandler
+#define ERPC_BOARD_LPI2C_INT_GPIO            GPIO1
+#define ERPC_BOARD_LPI2C_INT_PIN             0U
+#define ERPC_BOARD_LPI2C_INT_PIN_IRQ         PIN_INT1_IRQn
+#define ERPC_BOARD_LPI2C_INT_PIN_IRQ_HANDLER PIN_INT1_IRQHandler
 
 #if defined(__cplusplus)
 extern "C" {
@@ -231,6 +219,18 @@ status_t BOARD_LPI2C_Receive(LPI2C_Type *base,
                              uint8_t subaddressSize,
                              uint8_t *rxBuff,
                              uint8_t rxBuffSize);
+status_t BOARD_LPI2C_SendSCCB(LPI2C_Type *base,
+                              uint8_t deviceAddress,
+                              uint32_t subAddress,
+                              uint8_t subaddressSize,
+                              uint8_t *txBuff,
+                              uint8_t txBuffSize);
+status_t BOARD_LPI2C_ReceiveSCCB(LPI2C_Type *base,
+                                 uint8_t deviceAddress,
+                                 uint32_t subAddress,
+                                 uint8_t subaddressSize,
+                                 uint8_t *rxBuff,
+                                 uint8_t rxBuffSize);
 void BOARD_Accel_I2C_Init(void);
 status_t BOARD_Accel_I2C_Send(uint8_t deviceAddress, uint32_t subAddress, uint8_t subaddressSize, uint32_t txBuff);
 status_t BOARD_Accel_I2C_Receive(
@@ -239,6 +239,16 @@ void BOARD_Codec_I2C_Init(void);
 status_t BOARD_Codec_I2C_Send(
     uint8_t deviceAddress, uint32_t subAddress, uint8_t subAddressSize, const uint8_t *txBuff, uint8_t txBuffSize);
 status_t BOARD_Codec_I2C_Receive(
+    uint8_t deviceAddress, uint32_t subAddress, uint8_t subAddressSize, uint8_t *rxBuff, uint8_t rxBuffSize);
+void BOARD_Camera_I2C_Init(void);
+status_t BOARD_Camera_I2C_Send(
+    uint8_t deviceAddress, uint32_t subAddress, uint8_t subAddressSize, const uint8_t *txBuff, uint8_t txBuffSize);
+status_t BOARD_Camera_I2C_Receive(
+    uint8_t deviceAddress, uint32_t subAddress, uint8_t subAddressSize, uint8_t *rxBuff, uint8_t rxBuffSize);
+
+status_t BOARD_Camera_I2C_SendSCCB(
+    uint8_t deviceAddress, uint32_t subAddress, uint8_t subAddressSize, const uint8_t *txBuff, uint8_t txBuffSize);
+status_t BOARD_Camera_I2C_ReceiveSCCB(
     uint8_t deviceAddress, uint32_t subAddress, uint8_t subAddressSize, uint8_t *rxBuff, uint8_t rxBuffSize);
 #endif /* SDK_I2C_BASED_COMPONENT_USED */
 
